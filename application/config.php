5876cb6df9930427d9ba41571922e878b59fca47
umair <umair.makent@gmail.com>
2025-03-22 15:28:07 +0530

    merging

--- application/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$db_host	    = 'localhost';
-$db_user        = 'mbillsin_alabayav3_user';
-$db_password	= 'G6-4V!F$9hrx';
-$db_name	    = 'mbillsin_alabayav3';
-define('APP_URL', 'https://alabaya-v3.mbills.in');
-$_app_stage = 'Live'; // You can set this variable Live to Dev to enable ibilling Debug
